6d50b61036566f6ceae9aabaf600a73de69ed6a8
kudencagri <Kudencagri@gmail.com>
2024-08-12 16:48:39 +0300

    TestCase6 tamamlandı

--- src/test/java/StepDefinitions/_02_Steps.java
+++ src/test/java/StepDefinitions/_02_Steps.java
@@ -4,6 +4,7 @@ import Pages.LocatorPage;
 import Utilities.GWD;
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
@@ -24,5 +25,28 @@ public class _02_Steps {
             String choice=lists.get(i).get(1);
             lp.mySelect(element,choice);
         }
+    }
+
+    @Then("How many item in the basket")
+    public void howManyItemInTheBasket(DataTable dataTable) throws InterruptedException {
+        List<String> list=dataTable.asList(String.class);
+        for (String e:list){
+            String NumberStr= lp.getWebElement(e).getText();
+            String NuberOnly=NumberStr.replaceAll("[^0-9]","");
+            int Numb=Integer.parseInt(NuberOnly);
+            int deger=Numb %10;
+         // mustafaya sor
+            for (int j = 0; j < deger; j++) {
+                Thread.sleep(3000);
+                lp.myClick(lp.getGoCart());
+                lp.myClick(lp.getAction_Delete());
+                lp.myClick(lp.getAction_accept());
+                lp.myClick(lp.getLogo());
+            }
+
+        }
+
+
+
     }
 }
